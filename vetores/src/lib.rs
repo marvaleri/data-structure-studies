10342aa176d03dcc96177590107ec3921ea29292
Marlon <marlonsoares154@gmail.com>
2025-03-07 22:22:28 +0000

    hotfix: updating lib vetores, fixed struct MyVec

--- vetores/src/lib.rs
+++ vetores/src/lib.rs
@@ -1,3 +1,21 @@
+pub struct MyVec {
+    data: Vec<i32>,
+}
+
+impl MyVec{
+    pub fn new()-> MyVec {
+        MyVec {data: Vec::new()}
+    }
+
+    pub fn push(&mut self, value: i32) {
+        self.data.push(value);
+    }
+
+    pub fn get(&self, index: usize) -> Option<&i32> {
+        self.data.get(index)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -11,7 +29,6 @@ mod tests {
 
         assert_eq!(vec.get(0), Some(&10));
         assert_eq!(vec.get(1), Some(&20));
-        
         assert_eq!(vec.get(2), None);
     }
 }
