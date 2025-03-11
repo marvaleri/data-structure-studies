a7d8b8a9d577ec2a7b8e3054632017f65e7feacc
Marlon <marlonsoares154@gmail.com>
2025-03-11 23:54:36 +0000

    feat: new lib pilha and tests using stack

---
+++ pilha/target/debug/deps/pilha-39e766e0118f2664.d
@@ -0,0 +1,5 @@
+/workspaces/data-structure-studies/pilha/target/debug/deps/pilha-39e766e0118f2664: src/lib.rs
+
+/workspaces/data-structure-studies/pilha/target/debug/deps/pilha-39e766e0118f2664.d: src/lib.rs
+
+src/lib.rs:
---
+++ pilha/target/debug/deps/pilha-fc46d18c57c296dc.d
@@ -0,0 +1,7 @@
+/workspaces/data-structure-studies/pilha/target/debug/deps/libpilha-fc46d18c57c296dc.rmeta: src/lib.rs
+
+/workspaces/data-structure-studies/pilha/target/debug/deps/libpilha-fc46d18c57c296dc.rlib: src/lib.rs
+
+/workspaces/data-structure-studies/pilha/target/debug/deps/pilha-fc46d18c57c296dc.d: src/lib.rs
+
+src/lib.rs:
